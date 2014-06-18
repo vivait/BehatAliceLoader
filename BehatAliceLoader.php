3d6d4da396e2cb377f0ac0795293b650b1abec0e
Lewis Wright <lewiswright@gmail.com>
2014-06-18 12:25:45 +0100

    Create BehatAliceLoader.php

---
+++ BehatAliceLoader.php
@@ -0,0 +1,47 @@
+<?php
+namespace Vivait\BehatAliceLoader;
+
+use Behat\Gherkin\Node\TableNode;
+use Nelmio\Alice\Loader\Base;
+use Nelmio\Alice\Loader\Yaml;
+use Symfony\Component\Yaml\Inline;
+
+class BehatAliceLoader extends Yaml
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load($data)
+    {
+        if (is_array($data)) {
+            $value = reset($data);
+            if (count($data) === 1 && $value instanceOf TableNode) {
+                return $this->loadTableNode(key($data), $value);
+            }
+
+            return $this->loadArray($data);
+        }
+
+        return $this->loadFile($data);
+    }
+
+    public function loadArray($data) {
+        return Base::load($data);
+    }
+
+    public function loadFile($data) {
+        return parent::load($data);
+    }
+
+    public function loadTableNode($entity, TableNode $data) {
+        $hash = [];
+        // Parse any inline YAML inside a cell
+        foreach ($data->getHash() as $i => $row) {
+            foreach ($row as $j => $cell) {
+                $hash[$i][$j] = Inline::parse($cell);
+            }
+        }
+
+        return Base::load([$entity => $hash]);
+    }
+}
